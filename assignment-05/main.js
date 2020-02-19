ef2c053e7d421de07602ae9e19fa3f173f4d99db
kkredo <kylekredo@gmail.com>
2020-02-18 22:11:50 -0700

    added some

--- assignment-05/main.js
+++ assignment-05/main.js
@@ -1,15 +1,18 @@
+// having a hard time grasping this stuff, using example code heavily to help understand
 
 var imageTags = ["image1", "image2", "image3", "image4"];
-var image1 = dog.jpg;
-var image2 = tiger.jpg;
-var image3 = gofish.jpg;
-var image4 = gofish.jpg;
+imageTags[0] = dog.jpg;
+imageTags[1] = tiger.jpg;
+imageTags[2] = gofish.jpg;
+imageTags[3] = gofish.jpg;
+//maybe I should make the dog.jpg, tiger.... within the array?
 
 // create a variable with the blank image name
 var blankImagePath = "images/gofish.jpg";
 // create a empty array for the actual images
 var actualImages = new Array();
 
+//console log says printBlanks is not defined at onload
 function printBlanks()
 {
    // call our random image creation function
@@ -57,17 +60,19 @@ function createRandomImageArray()
 
 }
 
+
+//console log says flipImage is not defined
 function flipImage(number)
 {
 
-    document.getElementById(imageTags[1]).src= actualImages[0];
+    document.getElementById(imageTags[0]).src= actualImages[0];
         // this should be a quick function that just changes
         // the image based on what number was pressed
-    document.getElementById(imageTags[2]).src= actualImages[1];
+    document.getElementById(imageTags[1]).src= actualImages[1];
 
-    document.getElementById(imageTags[3]).src= actualImages[2];
+    document.getElementById(imageTags[2]).src= actualImages[2];
 
-    document.getElementById(imageTags[4]).src= actualImages[3];
+    document.getElementById(imageTags[3]).src= actualImages[3];
 
 
 }
