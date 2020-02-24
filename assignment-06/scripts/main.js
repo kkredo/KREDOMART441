698b54a40320ee16df8261fe47a5548bd476f861
kkredo <kylekredo@gmail.com>
2020-02-24 14:01:16 -0700

    swapped page

--- assignment-06/scripts/main.js
+++ assignment-06/scripts/main.js
@@ -106,7 +106,7 @@ function addToPlayer()
     player.lastname = lastName;
     player.age = age;
     localStorage.setItem("playerInfo", JSON.stringify(player));
-    window.location = "index.html";
+    window.location = "game.html";
 }
 
 // get the information out of JSON
