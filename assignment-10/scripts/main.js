73cc5f9cacb18e677ce95cf651cb7feeebbf817f
kkredo <kylekredo@gmail.com>
2020-03-30 16:27:45 -0600

    updated

---
+++ assignment-10/scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(this).keypress(function(event){
+        getKey(event);
+    });
+});
+
+function getKey(event)
+{
+    var char = event.which || event.keyCode;
+    var actualLetter = String.fromCharCode(char);
+}
